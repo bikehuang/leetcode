20d18fefe30caf5e852eda173d3f5fc6e8bd8184
bikehuang <hsy18627198712@163.com>
2019-08-09 11:36:42 +0800

    main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int b=10;
+int *p=&b;
+int a[8]={1,2,3,4,5,6,7,8};
+cout<<"a="<<a<<endl;
+cout<<"&a="<<&a<<endl;
+cout<<"p="<<p<<endl;
+cout<<"&p="<<&p<<endl;
+printf("hello");
+}
+
